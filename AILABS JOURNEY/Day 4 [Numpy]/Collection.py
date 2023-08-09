36da5e35c56010392fff3a5636b34aad74aa1f9c
ARITRA MUKHERJEE <121600347+AritraOfficial@users.noreply.github.com>
2023-08-09 16:18:12 +0530

    Add files via upload

---
+++ AILABS JOURNEY/Day 4 [Numpy]/Collection.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def add(a,b):
+    return a+b
+
+
+# In[3]:
+
+
+def multiply(a,b):
+    return a*b
+
+
+# In[ ]:
+
+
+def subtract(a,b):
+    return a-b
+
---
+++ AILABS JOURNEY/Intership_Journey/README.md
@@ -0,0 +1,2 @@
+# Intership_Journey
+Intership_Journey
